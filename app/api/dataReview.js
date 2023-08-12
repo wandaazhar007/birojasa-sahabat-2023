2069eec6e0dac292440c061599974ac76f59386f
wanda azhar <wandaazhar@gmail.com>
2023-08-12 13:17:33 -0400

    reviews page

---
+++ app/api/dataReview.js
@@ -0,0 +1,59 @@
+const reviewsArray = [
+  {
+    id: 1,
+    name: "Cani #05",
+    review: "Mantap si Bapak , reccommended pelayanannya..bisa dibilang paling cepat dan harga terjangkau ..terima kasih Pak 👍👍👍"
+  },
+  {
+    id: 2,
+    name: "Semoga Berkah",
+    review: "Proses cepat selesai.. aman... kurang lebih 6 jam sdh beres... terimakasih... semoga berkah"
+  },
+  {
+    id: 3,
+    name: "Sunar Conar",
+    review: "Terima kasih pak tatan yang telah membantu mutasi balik nama BPKB-STNK kendaraan saya, semoga berkah dan sukses terus untuk pak tatan usahanya.. Aamiin. Biro jasa yang recommended untuk yang mau mengurus perpanjangan, mutasi, balik nama BPKB-STNK.. Duduk manis dirumah, berkas dijemput dan beberapa hari/minggu selesai"
+  },
+  {
+    id: 4,
+    name: "Dani Ramdhani",
+    review: "ok banget...berkas diambil malem siang dah jd..awal coba2 aja..karna dah hampir habis pajak motor...tnyata mantul..info dr beliau bisa melayani jabodetabek..jawa jg ok...makasi n makin sukses y pak tatan.."
+  },
+  {
+    id: 5,
+    name: "Udin Berantakan",
+    review: "Biro Jasa ini Sungguh sangat Membantu di saat Pandemi Covid 19, Solusi Yang Sangat Baik Buat Teman-Teman serta Sanak Saudara yang kiranya ingin Membayar Pajak STNK ataupun Membalik Nama Kendaraan Solusi yang sangat Tepat dan Baik serta Pelayanan super Ramah, bahkan STNK kendaraan pun di Antarkan Ke Rumah.... Rekomendasi Sekali"
+  },
+  {
+    id: 6,
+    name: "Yohanes Christian",
+    review: "recommended, proses sangat jelas dan cepat, tidak sampai 1 hari sdh selesai berkas juga di antar jemput. terimakasih pak Tatan"
+  },
+  {
+    id: 7,
+    name: "Maja Pahit",
+    review: "Fast respon bangeet. Sehari jadi. Thanks yaa. Tanpa harus izin dari kantor. Sorenya udah selesai"
+  },
+  {
+    id: 8,
+    name: "Fichri I",
+    review: "Super cepat.. STNK diambil jam 10 pagi,, Selesai Diperpanjang magrib.👍👍"
+  },
+  {
+    id: 9,
+    name: "Subandi",
+    review: "Kalau jasa pembuatan paspor bisa ngga ya?"
+  },
+  {
+    id: 10,
+    name: "AA Adit",
+    review: "Pelayanannya cepat, makasih ya"
+  },
+  {
+    id: 10,
+    name: "Frio Anbia",
+    review: "Layananya cepat dan bagus"
+  },
+]
+
+export { reviewsArray };
---
+++ app/reviews/page.tsx
@@ -0,0 +1,9 @@
+import ReviewForPage from "../components/reviewForPage/ReviewForPage";
+
+const ReviewsPage: React.FC = () => {
+  return (
+    <ReviewForPage />
+  );
+}
+
+export default ReviewsPage;

0dd16651e68a237b763d1a268a53eb09ea136ae3
wanda azhar <wandaazhar@gmail.com>
2023-08-12 14:37:12 -0400

    privacy policy page

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,9 @@
+import PrivacyPolicyForPage from "../components/privacyPolicyForPage/PrivacyPolicyForPage";
+
+const PrivacyPage: React.FC = () => {
+  return (
+    <PrivacyPolicyForPage />
+  );
+}
+
+export default PrivacyPage;

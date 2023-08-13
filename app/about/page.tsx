131e23d08ad5c1ee086cc78132978655d62c66da
wanda azhar <wandaazhar@gmail.com>
2023-08-13 02:10:33 -0400

    fixing semantic html, and meta tag

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,14 @@
+import type { Metadata } from 'next';
 import Commitment from "../components/commitment/Commitment";
 import Founder from "../components/founder/Founder";
 import Mission from "../components/mission/Mission";
 import Story from "../components/story/Story";
 import Team from "../components/team/Team";
+
+export const metadata: Metadata = {
+  title: 'Tentang Kami - Birojasa Sahabat | Layanan Pengurusan STNK Terpercaya di Indonesia',
+  description: 'Selamat datang di halaman Tentang Kami Birojasa Sahabat. Kami adalah mitra tepercaya untuk pengurusan STNK di Indonesia. Dengan komitmen kami yang berfokus pada kemudahan dan kenyamanan pelanggan, Anda dapat mengandalkan layanan terbaik dari kami. Temukan solusi lengkap dan cepat #dikantorAja bersama kami',
+}
 
 const AboutPage: React.FC = () => {
   return (
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import type { Metadata } from 'next'
 import Hero from './components/hero/Hero'
 import CoverageArea from './components/coverageArea/CoverageArea'
 import Services from './components/services/Services'
@@ -9,6 +8,11 @@ import Choose from './components/choose/Choose'
 import AdsTwo from './components/adsTwo/AdsTwo'
 import FaqComponent from './components/faqComponent/FaqComponent'
 import ReviewComponent from './components/reviewComponent/ReviewComponent'
+
+export const metadata: Metadata = {
+  title: 'Birojasa Sahabat - Layanan Pengurusan STNK Terpercaya di Indonesia',
+  description: 'Selamat datang di Birojasa Sahabat! Kami siap membantu Anda dengan pengurusan STNK secara efisien dan terpercaya. Dapatkan kemudahan dalam mengurus berkas kendaraan Anda. Layanan cepat dan akurat #dikantorAja. Temukan solusi terbaik bersama kami.',
+}
 
 export default function Home() {
   return (

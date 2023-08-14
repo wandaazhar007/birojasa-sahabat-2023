25882860952d18e50070e8c549382a81089b5204
wanda azhar <wandaazhar@gmail.com>
2023-08-14 00:18:01 -0400

    Add measurement google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './styles/global.scss';
 import { Inter } from 'next/font/google'
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
+import GoogleAnalytics from './components/GoogleAnalytics';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <GoogleAnalytics GA_MEASUREMENT_ID='G-DH3PG8W6PJ' />
       <body className={inter.className}>
         <Navbar />
         {children}
